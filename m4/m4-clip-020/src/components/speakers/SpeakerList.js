1f42d0ebd4f4e87e3077dd7d6e60003e0dc7d4f1
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-02-24 15:17:58 +0100

    getting data from REST server

--- m4/m4-clip-020/src/components/speakers/SpeakerList.js
+++ m4/m4-clip-020/src/components/speakers/SpeakerList.js
@@ -1,7 +1,8 @@
 import SpeakerLine from "./SpeakerLine";
-import { speakerList } from "../../../speakersData";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-function List() {
+function List({speakers}) {
   const updatingId = 0; // 1269;
   const isPending = false;
 
@@ -35,7 +36,7 @@ function List() {
       </div>
 
       <div className="row g-3">
-        {speakerList.map(function (speakerRec) {
+        {speakers.map(function (speakerRec) {
           const highlight = false;
           return (
             <SpeakerLine
@@ -53,10 +54,25 @@ function List() {
 }
 
 const SpeakerList = () => {
-  const darkTheme = false;
+  const darkTheme = false; 
+  const [speakers, setSpeakers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getDataAsync() {
+      setLoading(true);
+      const results = await axios.get("/api/speakers"); 
+      setSpeakers(results.data);
+      setLoading(false);
+    }
+    getDataAsync();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+
   return (
     <div className={darkTheme ? "theme-dark" : "theme-light"}>
-      <List />
+      <List speakers={speakers} />
     </div>
   );
 };
